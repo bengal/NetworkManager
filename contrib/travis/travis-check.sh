a363c27f7ff1072da1854aea60253875394f484b
Lubomir Rintel <lkundrak@v3.sk>
2016-01-22 16:03:50 +0100

    travis: decouple the build from the test run
    (cherry picked from commit 1408b8c0a21105f3ea6d2e58d0fc03835f255d34)
    [bgalvani: dropped the coverity part, removed no-expect-messages from
    NMTST_DEBUG variable]

--- contrib/travis/travis-check.sh
+++ contrib/travis/travis-check.sh
@@ -8,18 +8,12 @@ print_test_logs() {
     echo ">>>> PRINT TEST LOGS $1 (done)"
 }
 
-sh ./autogen.sh \
-    --with-systemd-logind=no \
-    --enable-more-warnings=no
-
-make -j6
-
 if ! make check -k ; then
 
     print_test_logs "first-test"
 
     echo ">>>> RUN SECOND TEST (start)"
-    NMTST_DEBUG=TRACE,no-expect-messages make check -k || :
+    NMTST_DEBUG=TRACE make check -k || :
     echo ">>>> RUN SECOND TEST (done)"
 
     print_test_logs "second-test"
